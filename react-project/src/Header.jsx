cfd397f0928768afff89d57cfdb795c7a331be0d
ODaniel235 <domole235@gmail.com>
2024-02-06 00:46:36 +0100

    finishing header

--- react-project/src/Header.jsx
+++ react-project/src/Header.jsx
@@ -1,15 +1,43 @@
-import { NavLink } from "react-router-dom";
+import cartImage from "./assets/cart.svg";
+import accountImage from "./assets/account.svg";
 export default function Header() {
   return (
-/*     <>
-      <header className="bg-white p-6 flex justify-between h-fit w-screen z-10 top-0">
-        <div className="h-fit w-fit flex flex-col">
-            <div className="w-8 h-2 bg-black"></div>
-        </div>
-        <nav>
-          <ul></ul>
-        </nav>
-      </header>
-    </> */
+    <header className="flex h-fit w-screen bg-[rgb(255,255,255)] z-10 top-0 p-3 items-center justify-evenly shrink">
+      <div className="toggle h-fit w-fit lg:hidden">
+        <div className="bars"></div>
+        <div className="bars"></div>
+        <div className="bars"></div>
+      </div>
+      <h1 className="text-3xl text-black font-['inter']  font-extrabold">
+        SHOP.CO
+      </h1>
+      <nav className="flex md:hidden lg:flex">
+        <ul className="flex gap-3">
+          <li>
+            <select>
+              <option>Shop</option>
+              <option>Sale</option>
+            </select>
+          </li>
+          <li>On Sale</li>
+          <li>New Arrivals</li>
+          <li>Brands</li>
+        </ul>
+      </nav>
+
+      <input
+        type="search"
+        className="bg-[rgb(240,240,240)]  w-64  px-6 py-1 rounded-xl outline-none"
+        placeholder="Search..."
+      />
+      <div className="flex p-2 justify-evenly gap-2">
+        <button>
+          <img src={cartImage} alt="cart" />
+        </button>
+        <button>
+          <img src={accountImage} alt="profile" />
+        </button>
+      </div>
+    </header>
   );
 }
