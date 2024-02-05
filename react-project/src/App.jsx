69012aeff5b3329de0b5b9b8696177bee49f8e76
ODaniel235 <domole235@gmail.com>
2024-02-05 22:31:41 +0100

    resuming project

---
+++ react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/output.css";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ react-project/src/Header.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+export default function Header() {
+  return (
+/*     <>
+      <header className="bg-white p-6 flex justify-between h-fit w-screen z-10 top-0">
+        <div className="h-fit w-fit flex flex-col">
+            <div className="w-8 h-2 bg-black"></div>
+        </div>
+        <nav>
+          <ul></ul>
+        </nav>
+      </header>
+    </> */
+  );
+}
