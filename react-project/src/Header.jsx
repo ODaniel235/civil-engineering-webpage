ab3985c04bea03b56c50cd3fd3ad22c1d2881c9b
ODaniel235 <domole235@gmail.com>
2024-02-06 01:32:02 +0100

    mdq

--- react-project/src/Header.jsx
+++ react-project/src/Header.jsx
@@ -1,14 +1,10 @@
 import cartImage from "./assets/cart.svg";
 import accountImage from "./assets/account.svg";
+import searchImage from "./assets/search.svg";
 export default function Header() {
   return (
-    <header className="flex h-fit w-screen bg-[rgb(255,255,255)] z-10 top-0 p-3 items-center justify-evenly shrink">
-      <div className="toggle h-fit w-fit lg:hidden">
-        <div className="bars"></div>
-        <div className="bars"></div>
-        <div className="bars"></div>
-      </div>
-      <h1 className="text-xl lg:text-3xl text-black font-['inter']  font-extrabold">
+    <header className="flex h-fit w-screen bg-[rgb(255,255,255)] z-10 top-0 p-3 items-center justify-between lg:justify-evenly shrink">
+      <h1 className="text-2xl lg:text-3xl text-black font-['inter']  font-extrabold">
         SHOP.CO
       </h1>
       <nav className="hidden lg:flex">
@@ -30,7 +26,15 @@ export default function Header() {
         className="bg-[rgb(240,240,240)]  w-64  px-6 py-1 rounded-xl outline-none hidden md:flex"
         placeholder="Search..."
       />
-      <div className="flex p-2 justify-evenly gap-2">
+      <div className="toggle h-fit w-fit lg:hidden">
+        <div className="bars"></div>
+        <div className="bars"></div>
+        <div className="bars"></div>
+      </div>
+      <div className="lg:p-2 hidden lg:flex justify-evenly gap-2">
+        <button className=" lg:hidden">
+          <img src={searchImage} alt="Search" />
+        </button>
         <button>
           <img src={cartImage} alt="cart" />
         </button>
