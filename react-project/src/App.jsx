ce2fa0a86ab93866ab485e220581a69e771ef11e
ODaniel235 <domole235@gmail.com>
2024-02-16 18:06:08 +0100

    adding alternatives

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,19 +1,26 @@
-import Header from "./Header";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Authentication from "./Authentication";
 import NotePage from "./Notes";
 import FailedAuthentication from "./FailedAuthentication";
+import Header from "./Header";
 import civilEngineArray from "./FakeBackendApi";
 import "./styles/output.css";
 import { useState, useEffect } from "react";
 
 function App() {
   const [index, setIndex] = useState(-1);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
   const [regNumber, setRegNumber] = useState("");
   const regNumberRegex = /[a-z0-9]+/gi;
   const [checkValue, setCheckValue] = useState(false);
   const [justCheck, setJustCheck] = useState(true);
+
+  useEffect(() => {
+    // Save isLoggedIn state to localStorage
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
 
   const handleChange = (e) => {
     setRegNumber(
@@ -28,7 +35,7 @@ function App() {
       if (civilEngineArray[i].regNum === regNumber) {
         setIsLoggedIn(true);
         setJustCheck(false);
-        setCheckValue(false)
+        setCheckValue(false);
         setIndex(i);
         found = true;
         break;
@@ -44,15 +51,19 @@ function App() {
     setCheckValue(false);
     setJustCheck(true);
   };
-  const isLoggedFunction = ()=>{
-    setIsLoggedIn(true)
+
+  const isLoggedFunction = () => {
+    setIsLoggedIn(true);
     setJustCheck(false);
     setCheckValue(false);
-  }
+  };
 
   return (
     <BrowserRouter>
-      <Header img={isLoggedIn ? civilEngineArray[index].img : ""} isLoggedFunction={isLoggedFunction}>
+      <Header
+        img={isLoggedIn ? civilEngineArray[index].img : ""}
+        isLoggedFunction={isLoggedFunction}
+      >
         <Routes>
           {isLoggedIn ? (
             <>
