829a1c5805e2b2896e76606c1bd4a920922fefd6
ODaniel235 <domole235@gmail.com>
2024-02-24 21:19:37 +0100

    solving errors

--- react-project/src/Swiper.jsx
+++ react-project/src/Swiper.jsx
@@ -1,66 +1,62 @@
 import { useState } from "react";
-
 import civilEngineArray from "./FakeBackendApi";
 
 export default function SwiperMap(props) {
-  const [toggle, setToggle] = useState(false);
-  const civilArray = [...civilEngineArray];
-  const changeToggleOption = () => {
-    setToggle(!toggle);
+  const [toggleIndex, setToggleIndex] = useState(-1);
+
+  const handleToggle = (index) => {
+    setToggleIndex(index === toggleIndex ? -1 : index);
   };
+
   return (
-    <>
-      <section className="w-full flex justify-center my-12 mx-auto flex-wrap gap-7">
-        {civilArray
-          .filter((item, index) => index !== props.exclude && index !== 0)
-          .map((item, index) => (
+    <section className="w-full flex justify-center my-12 mx-auto flex-wrap gap-7">
+      {civilEngineArray.map(
+        (item, index) =>
+          index !== props.exclude &&
+          index !== 0 && (
             <div
               key={index}
-              onClick={() => toggle && setToggle(false)}
+              onClick={() => handleToggle(index)}
               className="w-full max-w-sm rounded-lg shadow bg-gray-800 border-gray-700"
             >
               <div className="flex justify-end px-4 pt-4">
                 <button
-                  id="dropdownButton"
-                  data-dropdown-toggle="dropdown"
                   className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
                   type="button"
-                  onClick={changeToggleOption}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleToggle(index);
+                  }}
                 >
                   <span className="sr-only">Open dropdown</span>
                   <svg
                     className="w-5 h-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
                     viewBox="0 0 16 3"
                   >
                     <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
                   </svg>
                 </button>
-                <div
-                  id="dropdown"
-                  className={`z-10 text-base absolute list-none  divide-y divide-gray-100 rounded-lg shadow w-44 bg-gray-700 ${
-                    !toggle && "hidden"
-                  }`}
-                >
-                  <ul className="py-2" aria-labelledby="dropdownButton">
-                    <li>
-                      <a
-                        href="#"
-                        className="no-underline text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                      >
-                        Export Data
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+                {toggleIndex === index && (
+                  <div className="z-10 text-base absolute list-none divide-y divide-gray-100 rounded-lg shadow w-44 bg-gray-700">
+                    <ul className="py-2" aria-labelledby="dropdownButton">
+                      <li>
+                        <a
+                          href="#"
+                          className="no-underline text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Export Data
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </div>
               <div className="flex flex-col items-center pb-10">
                 <img
                   className="w-24 h-24 mb-3 rounded-full shadow-lg"
                   src={item.img}
-                  alt="Bonnie image"
+                  alt={item.nickname}
                 />
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
                   {item.nickname}
@@ -69,23 +65,17 @@ export default function SwiperMap(props) {
                   {typeof item.position === "string" ? item.position : "100Lvl"}
                 </span>
                 <div className="flex mt-4 md:mt-6">
-                  <a
-                    href="#"
-                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
+                  <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     View Card
-                  </a>
-                  <a
-                    href="#"
-                    className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  >
+                  </button>
+                  <button className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                     Message
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
-          ))}
-      </section>
-    </>
+          )
+      )}
+    </section>
   );
 }
