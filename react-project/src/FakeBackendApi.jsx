40698c76eaf89e6c74917ca40dda95e3743d8f14
Omole Oluwadamilare Daniel <147058687+ODaniel235@users.noreply.github.com>
2024-02-28 13:35:53 -0800

    Update FakeBackendApi.jsx

--- react-project/src/FakeBackendApi.jsx
+++ react-project/src/FakeBackendApi.jsx
@@ -32,38 +32,44 @@ const civilEngineArray = [
     regNum: "202331082984ef",
     name: "Emmanuel Prince",
     img: "/pfp/emmanuel.jpg",
-    nickname: 'Prince'
+    nickname: "Prince",
   },
   {
     regNum: "202330657617ca",
     name: "Isaiah Christopher Sunday",
     img: "/pfp/isaiah.jpg",
-    nickname: 'Isaiah',
-    position: 'Course Rep'
+    nickname: "Isaiah",
+    position: "Course Rep",
+  },
+  {
+    regNum: "202330224342bf",
+    name: "Bright Emmanuel",
+    nickname: "Bright",
+    img: "/pfp/bright.jpg",
   },
   {
     regNum: "202330431180ja",
     name: "Chindah Joseph Chigozirim",
     img: "/pfp/chindah.jpg",
-    nickname: 'Joseph'
+    nickname: "Joseph",
   },
   {
     regNum: "202330588960ha",
     name: "Ihezou Daniel",
     img: "/pfp/seconddaniel.jpg",
-    nickname: 'Daniel'
+    nickname: "Daniel",
   },
   {
     regNum: "202330262150cf",
     name: "Edem Chizaram Daisy",
     img: "/pfp/zara.jpg",
-    nickname: 'Zara'
+    nickname: "Zara",
   },
   {
     regNum: "202330480899bf",
     name: "Israel Provost",
     img: "/pfp/pere.jpg",
-    nickname: 'Pere'
+    nickname: "Pere",
   },
 ];
 
