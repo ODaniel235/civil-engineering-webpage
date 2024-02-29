f1f5c613a000379622a9d8df14e48fb491accd34
Omole Oluwadamilare Daniel <147058687+ODaniel235@users.noreply.github.com>
2024-02-29 23:55:53 +0100

    Update Header.jsx

--- react-project/src/Header.jsx
+++ react-project/src/Header.jsx
@@ -158,7 +158,7 @@ export default function Header(props) {
             </div>
           </Disclosure.Panel>
           <div className="bg-[aliceblue]">
-            <div className="App min-h-screen p-2 max-w-7xl mx-auto overflow-x-hidden flex justify-center">
+            <div className="App min-h-screen p-2 max-w-7xl mx-auto flex justify-center">
               {props.children}
             </div>
           </div>
