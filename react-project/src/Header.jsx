06283ba882479ff0b1253953f69a5f6e3a9d974d
Omole Oluwadamilare Daniel <147058687+ODaniel235@users.noreply.github.com>
2024-02-29 22:23:00 +0100

    Update Header.jsx

--- react-project/src/Header.jsx
+++ react-project/src/Header.jsx
@@ -3,20 +3,13 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-
 const navigation = [
-  { name: "Dashboard", href: "/assign" },
-  { name: "Assignments", href: "/employees" },
-  { name: "Notes", href: "/notes" },
   { name: "Infos", href: "/infos" },
+  { name: "Notes", href: "/notes" },
 ];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Header(props) {
   const [active, setActive] = useState(0);
+
   const changeActive = (numberIndex) => {
     setActive(numberIndex);
   };
@@ -94,15 +87,15 @@ export default function Header(props) {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg- py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
+                            className={` no-underline ${
+                              (active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700")
+                            }`}
                           >
                             Your Profile
                           </a>
@@ -112,10 +105,10 @@ export default function Header(props) {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
+                            className={` no-underline ${
+                              (active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700")
+                            }`}
                           >
                             Settings
                           </a>
@@ -125,10 +118,10 @@ export default function Header(props) {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
+                            className={` no-underline ${
+                              (active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700")
+                            }`}
                           >
                             Sign out
                           </a>
