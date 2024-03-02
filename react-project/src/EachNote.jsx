e04bb73fafa93a6a97f3f9f55c1323a385ec8ec3
Omole Oluwadamilare Daniel <147058687+ODaniel235@users.noreply.github.com>
2024-03-02 13:48:17 +0100

    Update EachNote.jsx

--- react-project/src/EachNote.jsx
+++ react-project/src/EachNote.jsx
@@ -1,9 +1,14 @@
-export const GET101 = [{title: '', info: '', href: ''}] 
-export const GES101 = [{title: '', info: '', href: ''}] 
-export const GES101_1 = [{title: '', info: '', href: ''}] 
-export const GET111 = [{title: '', info: '', href: ''}] 
-export const CEE = [{title: '', info: '', href: ''}] 
+export const GET101 = [{ title: "", info: "", href: "" }];
+export const GES101 = [{ title: "", info: "", href: "" }];
+export const GES101_1 = [{ title: "", info: "", href: "" }];
+export const GET111 = [{ title: "", info: "", href: "" }];
+export const CEE = [{ title: "", info: "", href: "" }];
 export const ChemistryFiles = [
+  {
+    title: "Course Outline",
+    info: "Course Outline for the seemester",
+    href: "/Notes/Chemistry/Course-outline.rar",
+  },
   {
     title: "Atomic Theory",
     info: "Atomic theory in chemistry proposes that matter is composed of tiny, indivisible particles called atoms, which combine in specific ratios to form compounds. These atoms retain their identity during chemical reactions, and different elements have unique atomic structures and properties. Atomic theory serves as the foundation for understanding the behavior of matter and underpins numerous scientific advancements in fields such as chemistry, physics, and materials science.",
@@ -20,9 +25,9 @@ export const Math101Files = [
 ];
 export const Math102Files = [
   {
-    title: "",
-    info: "",
-    href: "",
+    title: "Course Outline",
+    info: "Course Outline for this first seemester",
+    href: "/Notes/Math120/Course-outline.rar",
   },
 ];
 export const PhysicsFiles = [
